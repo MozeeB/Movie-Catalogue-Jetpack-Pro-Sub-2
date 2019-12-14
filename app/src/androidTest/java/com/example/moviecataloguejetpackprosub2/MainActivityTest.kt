88689cc487f2289d26aaa53503daf2e4e7e7af65
mujibbur741 <mujiburr741@gmail.com>
2019-12-14 13:30:24 +0700

    Log Changes[submission 3] + clean code

--- app/src/androidTest/java/com/example/moviecataloguejetpackprosub2/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/moviecataloguejetpackprosub2/MainActivityTest.kt
@@ -6,7 +6,6 @@ import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import com.example.moviecataloguejetpackprosub2.helper.EspressoIdlingResource
