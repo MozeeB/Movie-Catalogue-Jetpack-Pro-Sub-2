3f47ba389d3d083b8987e2012f266d4e806c83fc
mujibbur741 <mujiburr741@gmail.com>
2019-12-17 10:37:11 +0700

    Log Changes[submission 3] + fixing nullable

--- app/src/main/java/com/example/moviecataloguejetpackprosub2/data/model/MoviesModel.kt
+++ app/src/main/java/com/example/moviecataloguejetpackprosub2/data/model/MoviesModel.kt
@@ -3,9 +3,8 @@ package com.example.moviecataloguejetpackprosub2.data.model
 
 class MoviesModel (
     val poster_path:String,
-
     val id:Int,
-    val backdrop_path:String,
+    val backdrop_path:String?,
     val original_title:String,
     val vote_average:Double,
     val overview:String,
